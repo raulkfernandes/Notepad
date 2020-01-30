bd1364f220d220c1c49ffd0b76f72cb57204e479
Raul Fernandes <raulkfernandes@gmail.com>
2020-01-30 19:06:46 -0300

    Looping NoteList items for test

--- app/src/main/java/br/com/alura/notepad/ui/activity/NoteListActivity.java
+++ app/src/main/java/br/com/alura/notepad/ui/activity/NoteListActivity.java
@@ -1,10 +1,14 @@
 package br.com.alura.notepad.ui.activity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.alura.notepad.R;
+import br.com.alura.notepad.dao.NoteDAO;
+import br.com.alura.notepad.model.Note;
+import br.com.alura.notepad.ui.adapter.NoteListAdapter;
 
 public class NoteListActivity extends AppCompatActivity {
 
@@ -12,5 +16,13 @@ public class NoteListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_list);
+
+        NoteDAO dao = new NoteDAO();
+        for (int i = 1; i < 10000; i++) {
+            dao.insert(new Note("New title " + i, "New description " + i));
+        }
+
+        ListView noteList = findViewById(R.id.activity_note_list_listview);
+        noteList.setAdapter(new NoteListAdapter(this, dao.getNoteList()));
     }
 }
