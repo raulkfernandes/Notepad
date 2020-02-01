9f63e9ef4aa2db081c21d31e955bdbb9fe0c15ff
Raul Fernandes <raulkfernandes@gmail.com>
2020-02-01 02:22:33 -0300

    Implementing InsertNote saving icon behaviour

--- app/src/main/java/br/com/alura/notepad/ui/activity/InsertNoteActivity.java
+++ app/src/main/java/br/com/alura/notepad/ui/activity/InsertNoteActivity.java
@@ -2,12 +2,19 @@ package br.com.alura.notepad.ui.activity;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.alura.notepad.R;
+import br.com.alura.notepad.dao.NoteDAO;
+import br.com.alura.notepad.model.Note;
 
 public class InsertNoteActivity extends AppCompatActivity {
+
+    NoteDAO dao = new NoteDAO();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +26,18 @@ public class InsertNoteActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_insert_note, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.menu_insert_note_icon) {
+            EditText noteTitle = findViewById(R.id.activity_insert_note_title);
+            EditText noteDescription = findViewById(R.id.activity_insert_note_description);
+
+            Note newNote = new Note(noteTitle.getText().toString(), noteDescription.getText().toString());
+            dao.insert(newNote);
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
