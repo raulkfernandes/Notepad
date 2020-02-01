8a3cf528e501c96238a5bf9b869b0261ad61dec1
Raul Fernandes <raulkfernandes@gmail.com>
2020-02-01 02:08:02 -0300

    Adding OptionsMenu to insert a new note

--- app/src/main/java/br/com/alura/notepad/ui/activity/InsertNoteActivity.java
+++ app/src/main/java/br/com/alura/notepad/ui/activity/InsertNoteActivity.java
@@ -1,8 +1,9 @@
 package br.com.alura.notepad.ui.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Menu;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import br.com.alura.notepad.R;
 
@@ -12,5 +13,11 @@ public class InsertNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insert_note);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_insert_note, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
